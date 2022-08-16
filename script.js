7d0847690dea6eab8dfdb9bcb3c941d14cea6586
Bogdan Todorov <90504727+bockoman@users.noreply.github.com>
2022-08-16 18:11:53 +0200

    problemche

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
