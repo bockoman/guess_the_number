7b7c896f29b7bf44ffbbb3c20ad70b6a5acfc83f
Bogdan Todorov <90504727+bockoman@users.noreply.github.com>
2022-08-17 23:49:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct number';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.message').textContent = 'Correct number';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
