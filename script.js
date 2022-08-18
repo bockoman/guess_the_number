fb8ee60cdc033a2bb4e5c689015f7566ed6b50bc
Bogdan Todorov <90504727+bockoman@users.noreply.github.com>
2022-08-18 18:28:47 +0200

    zavyrshena

--- script.js
+++ script.js
@@ -1,8 +1,65 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct number';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
+const secretNumberFunction = () => {
+  function reset() {
+    return 0;
+  }
+  return Math.round(Math.random() * (20 - 1) + 1);
+};
+let score = 20;
+let secretNumber = secretNumberFunction();
+let highscore = 0;
+let guessed = false;
 
-console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number.';
+  } else if (guess === secretNumber) {
+    guessed = true;
+    gameWon();
+    // proverqvame dali igraem oshte
+  } else if (!guessed) {
+    if (guess != secretNumber) {
+      if (score > 0) {
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = `You lost`;
+      }
+      if (guess < secretNumber) {
+        document.querySelector('.message').textContent = `Higher`;
+      } else {
+        document.querySelector('.message').textContent = `Lower`;
+      }
+    }
+  }
+});
+
+function gameWon() {
+  document.querySelector('.message').textContent = `Correct number`;
+  document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+  if (score > highscore) {
+    highscore = score;
+    document.querySelector('.highscore').textContent = highscore;
+  }
+}
+
+function reset() {
+  guessed = false;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('input').value = '';
+  document.querySelector('.message').textContent = `Start guessing`;
+  document.querySelector('body').style.backgroundColor = '#222';
+  secretNumber = secretNumberFunction();
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+}
+
+document.querySelector('.again').addEventListener('click', function () {
+  reset();
+});
